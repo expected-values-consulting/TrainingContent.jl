0873d690acc15cceecc651695450ab4dcdc0513d
Documenter.jl <documenter@juliadocs.github.io>
2024-06-03 23:13:41 +0000

    build based on 752797d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TrainingContent","category":"page"},{"location":"#TrainingContent","page":"Home","title":"TrainingContent","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TrainingContent.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TrainingContent]","category":"page"}]
+}
